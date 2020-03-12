531209eb90a1b6be515c54b90348d92a0ca10b00
Joaquim Pessôa Filho <joaquimp@gmail.com>
2020-03-12 14:05:34 -0300

    Estrutua de AG com inicialização da população

--- ReplyChallenge/GAbases.swift
+++ ReplyChallenge/GAbases.swift
@@ -17,7 +17,7 @@ enum Position {
 struct Individual {
     let developers:[Developer]
     let pms:[PM]
-    let fitness = 0
+    let fitness:Int
 }
 
 class Population {
@@ -29,21 +29,57 @@ class GA {
     let rows = 3
     let colls = 5
     var bestFitness = 0
-    let population: Population
-    var numberOfDevs = 0
-    var numberOfPMs = 0
+    let population: Population = Population()
+    let devs: [Developer]
+    let pms: [PM]
+    var numberOfDevPositions:Int = 0
+    var numberOfPMPositions:Int = 0
     
-    init() {
+    init(devs: [Developer], pms: [PM], pmAvailable:Int, devAvailable: Int) {
         room = [[.Unavailable, .Unavailable, .Unavailable, .Unavailable, .Unavailable],
                 [.Unavailable, .Developer(i: 0), .Unavailable, .Unavailable, .Developer(i: 1)],
                 [.Unavailable, .PM(i: 0), .PM(i: 1), .Developer(i: 2), .Developer(i: 3)]
         ]
-        self.numberOfPMs = 2
-        self.numberOfDevs = 4
+        
+        self.devs = devs
+        self.pms = pms
+        
+        self.numberOfPMPositions = pmAvailable
+        self.numberOfDevPositions = devAvailable
+        
+        self.createInitialPopulation()
     }
     
     private func createInitialPopulation() {
-        
+        for _ in 0...100 {
+            var iDevs:[Developer] = randDevs()
+            var iPMs:[PM] = randPMs()
+            
+            let fitness = self.fitnessCalculate(devs: iDevs, pms: iPMs)
+            let individual = Individual(developers: iDevs, pms: iPMs, fitness: fitness)
+            self.population.individual.append(individual)
+        }
+    }
+    
+    private func fitnessCalculate(devs:[Developer], pms:[PM]) -> Int {
+        return -1
+    }
+    
+    private func randDevs() -> [Developer] {
+        let shuffledDevs =  self.devs.shuffled()
+        return subArray(array: shuffledDevs, range: NSRange(location: 0, length: self.numberOfDevPositions))
+    }
+    
+    private func randPMs() -> [PM] {
+        let shuffledPMs =  self.pms.shuffled()
+        return subArray(array: shuffledPMs, range: NSRange(location: 0, length: self.numberOfPMPositions))
+    }
+    
+    func subArray<T>(array: [T], range: NSRange) -> [T] {
+      if range.location > array.count {
+        return []
+      }
+      return Array(array[range.location..<min(range.length, array.count)])
     }
 }
 
